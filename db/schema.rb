099e88f96063a2759733e184baaa69dbf4030783
gibsonbett <gibson.bett@student.moringaschool.com>
2022-11-10 09:46:29 +0300

    git commit -m "TD-9 <include user and note id in categories table>"

--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,14 @@ ActiveRecord::Schema.define(version: 2022_11_09_082429) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category"
-    t.integer "note_id"
     t.integer "user_id"
+    t.integer "note_id"
   end
 
   create_table "notes", force: :cascade do |t|
     t.string "note"
+    t.integer "category_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
