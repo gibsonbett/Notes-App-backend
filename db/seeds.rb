fb504fd6fab66bc73ec5348fdf3677233d5c4824
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-11-10 13:10:34 +0300

    git commit -m "TD-16 <seeding notes>"

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,20 @@ notes = Note.create(note:"finish project")
 
 category = Category.create(category:"school", user_id:user1.id, note_id:notes.id)
 
+# puts "Seeding notes..."
+# # run a loop 50 times
+# 50.times do |n|
+# # create a note with random data
+# note = Faker::Note.note
+# category_id = Faker::Note.category_id
+# user_id = Faker::Note.user_id
+# Note.create!(
+#     note: note,
+#     category_id: category_id,
+#     user_id: user_id
 
+   
+#   )
+# end
 
 puts "✅ Done seeding!"
