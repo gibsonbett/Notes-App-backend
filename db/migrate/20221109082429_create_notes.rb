06feea62af5bb6bbaed748775102de88fc631cb9
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-11-10 10:28:58 +0300

    git commit -m "TD-16 <fix bug>"

--- db/migrate/20221109082429_create_notes.rb
+++ db/migrate/20221109082429_create_notes.rb
@@ -2,6 +2,8 @@ class CreateNotes < ActiveRecord::Migration[6.1]
   def change
     create_table :notes do |t|
       t.string :note
+      t.integer :category_id
+      t.integer :user_id
     end
   end
 end
