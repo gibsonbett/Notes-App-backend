7a0242f7e0fb9ff2992f6a0d0edcc27c76879476
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-11-13 15:07:22 +0300

    edited relationships

--- db/migrate/20221109082429_create_notes.rb
+++ db/migrate/20221109082429_create_notes.rb
@@ -1,8 +1,8 @@
 class CreateNotes < ActiveRecord::Migration[6.1]
   def change
     create_table :notes do |t|
+      t.string :category
       t.string :todos
-      t.integer :category_id
       t.integer :user_id
     end
   end
